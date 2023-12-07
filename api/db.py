c69681186fb40b052ac72acaa8631e7d3030f4d2
kenny <kennyrimba@gmail.com>
2023-12-08 02:52:45 +0700

    f

--- api/db.py
+++ api/db.py
@@ -1,9 +1,10 @@
 import mysql.connector
-
+import os
+DB_HOST = os.getenv("DB_HOST", "serverless-1-instance-1.cbr7wdswraiy.ap-southeast-2.rds.amazonaws.com")
 
 def getMysqlConnection():
     return mysql.connector.connect(
-        user="root", host="localhost", port="3306", password="", database="pa_web"
+        user="root", host=DB_HOST, port="3306", password="", database="pa_web"
     )
 
 
