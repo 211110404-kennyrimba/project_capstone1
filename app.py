2c96330b82df616e7df4434949691ec0664143c1
kenny <kennyrimba@gmail.com>
2023-12-08 12:25:49 +0700

    f

--- app.py
+++ app.py
@@ -25,14 +25,17 @@ app = Flask(__name__)
 app.secret_key = "super secret key"
 #APIurl = "http://127.0.0.1:8000/api/"
 APIurl = os.getenv("API_URL", "http://0.0.0.0:8000/api/")
+DB_USER = os.getenv("DB_USER", "root")
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 
 def getMysqlConnection():
     return mysql.connector.connect(
-        user="root", host=DB_HOST, port="3306", password="", database="pa_web"
+        user=DB_USER, host=DB_HOST, port="3306", password="", database="pa_web"
     )
 print("DB HOST : ", DB_HOST)
+print("API_URL : ", APIurl)
+print("DB USER : ", DB_USER)
 
 class MyForm(FlaskForm):
     file = FileField(
