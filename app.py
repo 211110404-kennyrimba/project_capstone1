574807e25ddd061ad3176b7babe3e259ea7a72e7
kenny <kennyrimba@gmail.com>
2023-12-22 15:05:07 +0700

    f

--- app.py
+++ app.py
@@ -722,7 +722,7 @@ def userReservasi():
 
         cur.close()
         db.close()
-        return redirect(url_for(f"antrian/{id_user}"))
+        return redirect(url_for("promo"))
     else:
         cur.close()
         db.close()
